e2b6f9708febac77d2c95cba696abcb37d4b4f45
Yarrison allain <uwenayoallain@gmail.com>
2022-02-09 08:21:25 +0200

    some updates

---
+++ out/production/learnJava/downloads/rca.ac.rw/extracurricular.php
@@ -0,0 +1,446 @@
+
+<!DOCTYPE HTML>
+
+<html lang="en">
+
+    <head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    
+    
+    
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+    
+    <title>RCA | ExtraCurricular</title>
+    
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    
+    <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+    
+    <link rel="stylesheet" type="text/css" href="css/edua-icons.css">
+    
+    <link rel="stylesheet" type="text/css" href="css/animate.min.css">
+    
+    <link rel="stylesheet" type="text/css" href="css/owl.carousel.css">
+    
+    <link rel="stylesheet" type="text/css" href="css/owl.transitions.css">
+    
+    <link rel="stylesheet" type="text/css" href="css/cubeportfolio.min.css">
+    
+    <link rel="stylesheet" type="text/css" href="css/settings.css">
+    
+    <link rel="stylesheet" type="text/css" href="css/bootsnav.css">
+    
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    
+    <link rel="stylesheet" type="text/css" href="css/loader.css">
+    
+   <style>
+    h3{font-size: 22px; margin-bottom: 10px;}
+   </style>
+    
+    <link rel="icon" href="images/favicon.png">
+    
+    
+    
+    <!--[if lt IE 9]>
+    
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    
+    <![endif]-->
+    
+    </head>
+    
+    
+    
+    <body>
+    
+        <a href="#" class="scrollToTop"><i class="fa fa-angle-up"></i></a>
+        
+        <!--Loader-->
+        
+        <div class="loader">
+        
+          <div class="bouncybox">
+        
+              <div class="bouncy"></div>
+        
+            </div>
+        
+        </div>
+        
+        
+         <!--TOPBAR-->
+        
+        <div class="topbar">
+
+  <div class="container">
+
+    <div class="row">
+
+      <div class="col-md-12">
+
+        <div class="pull-left">
+
+        <span class="info"><a href="#.">Contact Us: </a></span>
+
+        <span class="info"><i class="icon-phone2"></i>(250) 788-548-000</span>
+
+        <span class="info"><i class="icon-mail"></i>info@rca.ac.rw</span>
+
+        </div>
+
+        <ul class="social_top pull-right">
+
+          <li><a target="new" href="https://rcagroupworkspace.slack.com/messages/CG3A11ZNG/team/UG5KU4JQ2/"><i class="fa fa-slack"></i></a></li>
+          
+          <li><a target="new" href="https://facebook.com/Rwanda-Coding-Academy-RCA-112111193979259"><i class="fa fa-facebook"></i></a></li>
+
+          <li><a target="new" href="https://twitter.com/RwCodingAcademy"><i class="icon-twitter4"></i></a></li>
+
+
+        </ul>
+
+      </div>
+
+    </div>
+
+  </div>
+
+</div>        
+        <!--TOPBAR ends-->
+        
+        
+        
+        <!--Header-->
+        
+        <header>
+        
+          <nav class="navbar navbar-default navbar-fixed white no-background bootsnav">
+        
+            <div class="container"> 
+        
+               <div class="search_btn btn_common"><i class="icon-icons185"></i></div>
+        
+                  <div class="navbar-header">
+            
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-menu">
+            
+                      <i class="fa fa-bars"></i>
+            
+                    </button>
+            
+                    <a class="navbar-brand" href="./">
+                        <img src="images/logo-white-rca.png" alt="logo" class="logo logo-display">
+                        <img src="images/logo-rca.png" class="logo logo-scrolled" alt="">
+            
+                    </a>
+            
+                  </div>
+        
+              <div class="collapse navbar-collapse" id="navbar-menu">
+        
+                <ul class="nav navbar-nav navbar-right" data-in="fadeInDown" data-out="fadeOut">
+   
+           		   <li><a href="./">Home</a></li>
+
+                   <li><a href="about-us.php">About Us</a></li> 
+                   
+                   
+        		   <li>
+        		       
+        		       <a href="./downloads/Babyeyi-2022.pdf">Babyeyi</a>
+        		       
+        		  </li> 
+        		  
+           		  
+        		   <li ><a href="admissions.php">Admissions</a></li>
+        
+        		   <li class="active">
+        		       
+        		       <a href="extracurricular.php">ExtraCurricular</a>
+        		       
+        		  </li>         
+        		 
+        		   <li>
+        		       
+        		       <a target="new" href="https://moodle.benax.rw">Moodle</a>
+        		       
+        		  </li>        		  
+
+        		   <li>
+        		       
+        		       <a target="new" href="https://foreach.benax.rw">forEach</a>
+        		       
+        		  </li>    
+        		  
+                </ul>
+        
+              </div>
+        
+            </div>   
+        
+          </nav>
+        
+        </header>
+        
+    
+        
+        <!--Search-->
+        
+        <div id="search">
+        
+          <button type="button" class="close">×</button>
+        
+          <form><input type="hidden" name="ptSignature" value="QTSa8hqw+R3qylxFNMtv1Yu0lvXMMaQwjj8C6xkXY2w5kS/uufZq6Ehr5Vrkn/x/" />
+<input type="hidden" name="csSignature" value="/0FG2CmRF1wLwz1cCi8Bwg==" />
+
+        
+            <input type="search" value="" placeholder="Search here...."  required/>
+        
+            <button type="submit" class="btn btn_common blue">Search</button>
+        
+          </form>
+        
+        </div>
+
+
+
+
+
+
+<!--Page Header-->
+
+<section class="page_header padding-top">
+
+  <div class="container">
+
+    <div class="row">
+
+      <div class="col-md-12 page-content">
+
+        <h1>ExtraCurricular</h1>
+
+        <p>
+
+		</p>
+
+        <div class="page_nav">
+
+      <span>You are here:</span> <a href="./">Home</a> <span><i class="fa fa-angle-double-right"></i>ExtraCurricular</span>
+
+      </div>
+
+      </div>
+
+    </div>
+
+  </div>
+
+</section>
+
+
+
+
+
+<!--Contact Deatils -->
+
+<section id="contact" class="padding">
+
+  <div class="container">
+
+
+
+
+        <h2 class="heading heading_space">Club for "Artificial Intelligence, Robotics, IoT"<span class="divider-left"></span></h2>
+      
+        <div style="font-size: 20px; width: 100%; ">  
+			<p>
+				The club was founded by Engineer Gabriel Baziramwabo in April 2019.
+				During his Master's studies in Kobe Institute of Computing (KIC), Japan (2016-2018), 
+				Gabriel specialized in Educational Robotics Engineering and Software Development.
+				<br>
+				<a style="color: #4b0688" href="https://www.youtube.com/watch?v=xdEeQtQlMHA">
+				    Watch a video about Gabriel's Master's Project on Robotics for Young Rwandans
+				</a>
+				
+				
+			</p>
+			
+		<p>
+		    <img style="width: 100%; border-radius:  10px" src="images/robotics-001.jpg">	 
+		</p>
+			
+		<br>	
+		<h3>Aim of the Club</h3>
+		<p>
+			 The club aims at engaging members in Artificial Intelligence, Robotics, and IoT.<br>
+			 Because the curriculum is limited to some specific courses, this is a chance to actively explore how 
+			 trending technologies work. 
+		</p>
+		
+
+		
+		<h3>Club Activities</h3>
+		<p>
+			 Activities include but not limited to designing and building robots, engaging with Artificial Intelligence
+			 algorithms like face recognition, plant disease detections, and much more...
+		</p>
+
+
+
+  </div>
+
+</section>
+
+<!--Contact Deatils -->
+
+
+
+
+
+
+<!--FOOTER-->
+
+<footer class="padding-top">
+
+  <div class="container">
+
+    <div class="row">
+
+      <div class="col-md-4 col-sm-4 footer_panel bottom25">
+
+        <h3 class="heading bottom25">About Us<span class="divider-left"></span></h3>
+
+        <a href="./" class="footer_logo bottom25"><img src="images/motto-rca.png" alt="RCA"></a>
+
+        <p>We are a special model school incepted by the Government of Rwanda 
+to teach Software Development, Embedded Systems Programming,  and Cyber-Security.<br> 
+<ul class="links">
+<li><a href="about-us.php">Read more about us<i style="margin-left: 3px" class="icon-chevron-small-right"></i></a></li>
+</ul>
+</p>        
+         <ul class="social_icon top25">
+
+          <li><a target="new" href="https://rcagroupworkspace.slack.com/messages/CG3A11ZNG/team/UG5KU4JQ2/" class="slack"><i class="fa fa-slack"></i></a></li>
+ 
+          <li><a target="new" href="https://facebook.com/Rwanda-Coding-Academy-RCA-112111193979259" class="facebook"><i class="fa fa-facebook"></i></a></li>
+
+          <li><a target="new" href="https://twitter.com/RwCodingAcademy" class="twitter"><i class="icon-twitter4"></i></a></li>
+
+
+
+        </ul>
+
+      </div>
+
+      <div class="col-md-4 col-sm-4 footer_panel bottom25">
+
+        <h3 class="heading bottom25">Quick Links<span class="divider-left"></span></h3>
+
+        <ul class="links">
+
+          <li><a href="./"><i class="icon-chevron-small-right"></i>Home</a></li>
+
+          <li><a href="about-us.php"><i class="icon-chevron-small-right"></i>About Us</a></li>
+
+          <li><a href="admissions.php"><i class="icon-chevron-small-right"></i>Admissions</a></li>
+          
+          <li><a href="extracurricular.php"><i class="icon-chevron-small-right"></i>ExtraCurricular</a></li>
+          
+          <li><a href="#."><i class="icon-chevron-small-right"></i>Instructors</a></li>
+
+          <li><a href="#."><i class="icon-chevron-small-right"></i>Contact Us</a></li>
+
+        </ul>
+
+      </div>
+
+      <div class="col-md-4 col-sm-4 footer_panel bottom25">
+
+        <h3 class="heading bottom25">Keep in Touch <span class="divider-left"></span></h3>
+
+        <p class=" address"><i class="icon-map-pin"></i>Musanze-Rubavu Road, Mukamira Sector, Nyabihu District, Rwanda</p>
+
+        <p class=" address"><i class="icon-phone"></i>((250) 788-548-000</p>
+
+        <p class=" address"><i class="icon-mail"></i><a href="mailto:info@rca.ac.rw">info@rca.ac.rw</a></p>
+
+        <img src="images/footer-map-rca.png" alt="we are here" class="img-responsive">
+
+      </div>
+
+    </div>
+
+  </div>
+
+</footer>
+
+<div class="copyright">
+
+  <div class="container">
+
+    <div class="row">
+
+      <div class="col-md-12 text-center">
+
+        <p>Copyright &copy; 2022<a href="./"> Rwanda Coding Academy</a>. All rights reserved.</p>
+
+      </div>
+
+    </div>
+
+  </div>
+
+</div>
+
+<!--FOOTER ends-->
+
+
+
+<script src="js/jquery-2.2.3.js"></script>
+
+<script type="text/javascript" src="http://maps.google.com/maps/api/js?key=AIzaSyAOBKD6V47-g_3opmidcmFapb3kSNAR70U"></script>
+
+<script src="js/gmaps.min.js"></script>
+
+<script src="js/bootstrap.min.js"></script>
+
+<script src="js/bootsnav.js"></script>
+
+<script src="js/jquery.appear.js"></script>
+
+<script src="js/jquery-countTo.js"></script>
+
+<script src="js/jquery.parallax-1.1.3.js"></script>
+
+<script src="js/owl.carousel.min.js"></script>
+
+<script src="js/jquery.cubeportfolio.min.js"></script>
+
+<script src="js/jquery.themepunch.tools.min.js"></script>
+
+<script src="js/jquery.themepunch.revolution.min.js"></script>
+
+<script src="js/revolution.extension.layeranimation.min.js"></script>
+
+<script src="js/revolution.extension.navigation.min.js"></script>
+
+<script src="js/revolution.extension.parallax.min.js"></script>
+
+<script src="js/revolution.extension.slideanims.min.js"></script>
+
+<script src="js/revolution.extension.video.min.js"></script>
+
+<script src="js/wow.min.js"></script>
+
+<script src="js/functions.js"></script>
+
+
+
+</body>
+
+</html>
+
